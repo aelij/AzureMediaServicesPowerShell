77188842a0e068e3b93aeaf1ac8edec843be18e0
Rob Blackwell <rob.blackwell@robblackwell.org.uk>
2012-10-10 17:42:46 +0200

    Task Presets

---
+++ TaskPresets/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Two10.MediaServices;
+
+namespace TaskPresets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var taskPreset in Constants.taskPresets)
+            {
+                Console.WriteLine(taskPreset);
+            }
+        }
+    }
+}
