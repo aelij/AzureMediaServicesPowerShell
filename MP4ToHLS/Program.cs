f60401aae79022ca1ca00b889cb3bad1da2aa3b1
Rob Blackwell <rob.blackwell@robblackwell.org.uk>
2013-03-19 16:53:38 +0000

    Liams HLS stuff

--- MP4ToHLS/Program.cs
+++ MP4ToHLS/Program.cs
@@ -1,4 +1,21 @@
-﻿using Microsoft.WindowsAzure.MediaServices.Client;
+﻿#region Copyright (c) 2012 - 2013 Two10 Degrees Ltd
+//
+// (C) Copyright 2012 - 2013 Two10 Degrees Ltd
+//      All rights reserved.
+//
+// This software is provided "as is" without warranty of any kind,
+// express or implied, including but not limited to warranties as to
+// quality and fitness for a particular purpose. Two10 Degrees Ltd
+// does not support the Software, nor does it warrant that the Software
+// will meet your requirements or that the operation of the Software will
+// be uninterrupted or error free or that any defects will be
+// corrected. Nothing in this statement is intended to limit or exclude
+// any liability for personal injury or death caused by the negligence of
+// Two10 Degrees Ltd, its employees, contractors or agents.
+//
+#endregion
+
+using Microsoft.WindowsAzure.MediaServices.Client;
 using System;
 using Two10.MediaServices;
 
@@ -40,7 +57,7 @@ namespace MP4ToHLS
             job.Submit();
 
             Console.WriteLine(job.Id);
-            Console.WriteLine("Once job is complete, retrieve StreamingURL and append (format=m3u8-aapl)";
+            Console.WriteLine("Once job is complete, retrieve StreamingURL and append (format=m3u8-aapl)");
             return 0;
         }
     }
